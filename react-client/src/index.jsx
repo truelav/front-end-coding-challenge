52ce7dd2e8acd5ffe207d5104b364536da3d73ec
Serghei Madan <madan.serghei@yahoo.com>
2018-03-06 09:32:37 -0800

    [json-server] getting posts to the frontEnd

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: [],
+    }
+  this.getPosts = this.getPosts.bind(this)  
+  }
+
+  componentDidMount () {
+    this.getPosts()
+  }
+
+  getPosts() {
+   axios.get('http://localhost:3000/db')
+    .then( (response) => {
+      console.log(response)
+      this.setState({
+        tasks: response.data
+      })
+    })
+    .catch(function (err) {
+      console.log(err)
+    });
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        
+      </div>
+    )
   }
 }
 
